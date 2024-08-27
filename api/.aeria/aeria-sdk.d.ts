c7e434cc23449ac0a8e1f26a01096c14e86a4767
ringeringeraja <joaosan177@gmail.com>
2024-08-27 02:36:08 -0300

    refactor: update to newer version

--- api/.aeria/aeria-sdk.d.ts
+++ api/.aeria/aeria-sdk.d.ts
@@ -251,13 +251,6 @@ declare type MirrorDescriptions = {
           "type"
         ]
       },
-      "comments": {
-        "type": "array",
-        "items": {
-          "$ref": "comment",
-          "inline": true
-        }
-      },
       "created_at": {
         "type": "string",
         "format": "date-time",
@@ -281,6 +274,9 @@ declare type MirrorDescriptions = {
         "indexes": [
           "name"
         ]
+      },
+      "comments": {
+        "readOnly": true
       }
     },
     "icon": "ticket",
--- api/src/collections.ts
+++ api/src/collections.ts
@@ -1,6 +1,6 @@
 export * from "../.aeria/out/collections/index.mjs";
-import { extendTicketCollection, Ticket } from "../.aeria/out/index.mjs";
-import { get, getAll, Result, throwIfError } from "aeria";
+import { extendTicketCollection } from "../.aeria/out/index.mjs";
+import { throwIfError } from "aeria";
 
 export const ticket = extendTicketCollection({
   description: {
@@ -17,45 +17,20 @@ export const ticket = extendTicketCollection({
         },
       },
     },
-  },
-  functions: {
-    get: async (payload, context) => {
-      const { error, result: ticket } = await get(payload, context);
-      if (error) {
-        return Result.error(error);
+    properties: {
+      comments: {
+        getter: async (document, context): Promise<unknown> => {
+          return throwIfError(await context.collections.comment.functions.getAll({
+            filters: {
+              ticket: document._id
+            },
+            populate: [
+              'owner',
+              'images',
+            ],
+          }))
+        }
       }
-
-      ticket.comments = throwIfError(
-        await context.collections.comment.functions.getAll({
-          filters: {
-            ticket: ticket._id,
-          },
-        })
-      );
-
-      return Result.result(ticket);
-    },
-    getAll: async (payload, context) => {
-      const { error, result } = await getAll(payload, context);
-      if (error) {
-        return Result.error(error);
-      }
-
-      const tickets: Ticket[] = [];
-      for (const ticket of result) {
-        tickets.push({
-          ...ticket,
-          comments: throwIfError(
-            await context.collections.comment.functions.getAll({
-              filters: {
-                ticket: ticket._id,
-              },
-            })
-          ),
-        });
-      }
-
-      return Result.result(tickets);
-    },
+    }
   },
 });
